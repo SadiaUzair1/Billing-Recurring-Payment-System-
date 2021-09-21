b5725c4c22b3deb536aabf57343d197d8537ea29
Your Name <sadia.uzair@devsinc.com>
2021-09-21 17:01:32 +0500

    changed configurations

--- Gemfile
+++ Gemfile
@@ -63,8 +63,8 @@ group :development, :test do
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'delayed_job_active_record'
-gem 'devise_invitable'
-
+gem 'devise_invitable', '~> 2.0', '>= 2.0.5'
+gem 'figaro', '~> 1.2'
 gem 'pry', '~> 0.14.1'
 gem 'stripe'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,14 +34,14 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
-
+  config.serve_static_assets = true
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
+  config.assets.digest = true
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
