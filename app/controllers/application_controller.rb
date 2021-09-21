0d0db1c101cf978a90473ca30d3087b355801a28
Your Name <sadia.uzair@devsinc.com>
2021-09-21 11:48:02 +0500

    Configuring pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
+  before_action :authenticate_user!, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
