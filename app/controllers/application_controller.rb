6e8b5c0ee5786a375331f2971c3f1ad429de3395
Your Name <sadia.uzair@devsinc.com>
2021-09-21 11:56:01 +0500

    Fix devise error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 
 class ApplicationController < ActionController::Base
   include Pundit
-  before_action :authenticate_user!, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
