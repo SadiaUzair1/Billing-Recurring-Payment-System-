6f2dca5a50408b4897142fb873ab98d9dc741e23
Your Name <sadia.uzair@devsinc.com>
2021-09-22 13:58:08 +0500

    changed api key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,4 +4,4 @@ Rails.configuration.stripe = {
   publishable_key: Rails.application.secrets.stripe_publishable_key,
   secret_key: Rails.application.secrets.stripe_secret_key
 }
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+stripe.api_key = ENV['stripe_secret_key']
