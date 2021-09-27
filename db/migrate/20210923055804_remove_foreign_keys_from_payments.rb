2d1acf4934706a3c3d43f65c53b28d6bcdf6c135
Your Name <sadia.uzair@devsinc.com>
2021-09-27 13:08:24 +0500

    removing_table

--- db/migrate/20210923055804_remove_foreign_keys_from_payments.rb
+++ db/migrate/20210923055804_remove_foreign_keys_from_payments.rb
@@ -2,9 +2,6 @@
 
 class RemoveForeignKeysFromPayments < ActiveRecord::Migration[6.1]
   def change
-    drop_table :payments_and_plans_ids do |t|
-      t.string :plan_id
-      t.string :payment_id
-    end
+
   end
 end
