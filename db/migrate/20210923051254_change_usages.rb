a141937c7b12c63e1635e5d074a935418f1a140f
Your Name <sadia.uzair@devsinc.com>
2021-09-27 12:01:25 +0500

    removed max limit atribyte

--- db/migrate/20210923051254_change_usages.rb
+++ db/migrate/20210923051254_change_usages.rb
@@ -4,7 +4,6 @@ class ChangeUsages < ActiveRecord::Migration[6.1]
   def change
     change_table(:usages, bulk: true) do |_t|
       remove_column(:usages, :amount, :integer)
-      remove_column(:usages, :max_unit_limit, :integer)
       remove_column(:usages, :feature_name, :string)
       remove_column(:usages, :features_name, :string)
       remove_column(:usages, :plan_name, :string)
