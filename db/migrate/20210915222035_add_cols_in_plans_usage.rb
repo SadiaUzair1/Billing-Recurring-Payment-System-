46448643536c1725bbeb1730c7fb2b26069d20c7
Your Name <sadia.uzair@devsinc.com>
2021-09-27 11:50:14 +0500

    removed attribute

--- db/migrate/20210915222035_add_cols_in_plans_usage.rb
+++ db/migrate/20210915222035_add_cols_in_plans_usage.rb
@@ -3,7 +3,6 @@
 class AddColsInPlansUsage < ActiveRecord::Migration[6.1]
   def change
     change_table :plan_usages, bulk: true do |t|
-      t.integer :feature_max_limit, null: false, default: 0
       t.integer :increased_units, null: false, default: 1
       t.integer :amount, null: false, default: 0
       t.string :features_name, null: false, default: ''
